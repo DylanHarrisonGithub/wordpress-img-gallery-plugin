6509b61445b5c65d5882625717d59374ba0ce56f
DylanHarrisonGithub <dillysnaps@gmail.com>
2019-09-15 23:14:22 -0500

    get img src by id and generate img element

--- wordpress-img-gallery-plugin.php
+++ wordpress-img-gallery-plugin.php
@@ -8,11 +8,17 @@
   add_shortcode('wp_img_gallery', function($atts = [], $content = null, $tag = '') {
     $atts = array_change_key_case((array)$atts, CASE_LOWER);
     if (array_key_exists('ids', $atts)) {
-      $content = "";
+      $content = "<div>";
       $ids = explode(',', $atts['ids']);
       foreach ($ids as $key => $value) {
-        $content .= '<p>'.$value.'</p>';
+        $img = wp_get_attachment_image_src($ids[$key]);
+        if ($img) {
+          $content .= "<img src='{$img[0]}' onclick='location.href=\"{$img[0]}\"'>";
+        } else {
+          $content .= "<span>"."Image id: ".$ids[$key]." not found!"."</span>";
+        }
       }
+      $content .="</div>";
       return $content;
     } else {
       return 'Empty Gallery';
